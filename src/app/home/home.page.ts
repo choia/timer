aa11b804ffd2ac38c67081b8ed524c2603327f42
Alex Choi <alex.ji.choi@gmail.com>
2020-10-12 20:45:25 -0400

    add android platform packages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: [ 'home.page.scss' ]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+	constructor(private localNotifications: LocalNotifications) {}
 
-  constructor() {}
+	simpleNotify() {
+		this.localNotifications.schedule({
+			id: 1,
+			title: 'This is first notification',
+			text: 'Simple local notifications'
+		});
+	}
 
+	async ngOnInit() {}
 }
